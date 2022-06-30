24439c49fdcc40f7eae4ba1e20d1b516f263ba10
DmitryGaydabura <gaydabura.d@icloud.com>
2022-06-30 23:07:14 +0300

    HW Lesson 3 added

---
+++ src/com/example/lesson3/myLinkedList/Main.java
@@ -0,0 +1,37 @@
+package com.example.lesson3.myLinkedList;
+public class Main {
+
+    public static void main(String[] args) {
+        /* Available methods:
+         * add
+         * size
+         * peekFirst
+         * peekLast
+         * add
+         * getLast
+         * getFirst
+         * addLast
+         * addFirst
+         * remove
+         * set
+         * */
+
+        MyLinkedList myLinkedList = new MyLinkedList();
+
+        for (int i = 0; i < 10; i++) {
+            myLinkedList.add((int) (Math.random() * 10));
+        }
+
+        printMyList(myLinkedList);
+
+    }
+
+    private static void printMyList(MyLinkedList myLinkedList) {
+        for (int i = 0; i < myLinkedList.size; i++) {
+            System.out.print(myLinkedList.get(i) + " ");
+        }
+        System.out.println("\n");
+    }
+
+
+}
