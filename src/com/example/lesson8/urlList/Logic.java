9845127d31df2c46e31573471179424eb3e0cff8
DmitryGaydabura <gaydabura.d@icloud.com>
2022-07-16 02:54:25 +0300

    HW Lesson 8 added URL list

--- src/com/example/lesson8/urlList/Logic.java
+++ src/com/example/lesson8/urlList/Logic.java
@@ -6,22 +6,25 @@ import java.util.*;
 
 
 public class Logic {
-    public static void start() throws FileNotFoundException {
-        ArrayList<String> everyURLList = createListOfAllURL();
-        normalizeURL(everyURLList);
-        HashMap<String,Integer> howManyTimesDoesItAppear = createMapOfAllURL(everyURLList);
-        sortMapByValueAndPrint(howManyTimesDoesItAppear);
 
+    public static ArrayList<String> createListOfAllURL() throws FileNotFoundException {
+        File file = new File("src/com/example/lesson8/urlList/urls");
+        Scanner in = new Scanner(file);
+        ArrayList<String> everyURL = new ArrayList<>();
+
+        // Adding every line in the file to the ArrayList.
+        while (in.hasNextLine()) {
+            everyURL.add(in.nextLine());
+        }
+        return everyURL;
     }
 
-    private static void sortMapByValueAndPrint(HashMap<String, Integer> howManyTimesDoesItAppear) {
-        howManyTimesDoesItAppear.entrySet().stream()
-                .sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
-                .forEach(System.out::println);
+    public static void normalizeURL(ArrayList<String> everyURLList) {
+        everyURLList.replaceAll(s -> s.split("/")[0]);
     }
 
-    private static HashMap<String,Integer> createMapOfAllURL(ArrayList<String> everyURLList) {
-        HashMap<String,Integer> howManyTimesDoesItAppear = new HashMap<>();
+    public static HashMap<String, Integer> createMapOfAllURL(ArrayList<String> everyURLList) {
+        HashMap<String, Integer> howManyTimesDoesItAppear = new HashMap<>();
         for (String s : everyURLList) {
             if (!howManyTimesDoesItAppear.containsKey(s)) {
                 howManyTimesDoesItAppear.put(s, 1);
@@ -33,19 +36,9 @@ public class Logic {
         return howManyTimesDoesItAppear;
     }
 
-    private static void normalizeURL(ArrayList<String> everyURLList) {
-        everyURLList.replaceAll(s -> s.split("/")[0]);
-    }
-
-    private static ArrayList<String> createListOfAllURL() throws FileNotFoundException {
-        File file = new File("src/com/example/lesson8/urlList/urls");
-        Scanner in = new Scanner(file);
-
-
-        ArrayList<String> everyURL = new ArrayList<>();
-        while (in.hasNextLine()) {
-            everyURL.add(in.nextLine());
-        }
-        return everyURL;
+    public static void sortMapByValueAndPrint(HashMap<String, Integer> howManyTimesDoesItAppear) {
+        howManyTimesDoesItAppear.entrySet().stream()
+                .sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
+                .forEach(System.out::println);
     }
 }
--- src/com/example/lesson8/urlList/Main.java
+++ src/com/example/lesson8/urlList/Main.java
@@ -1,9 +1,22 @@
 package com.example.lesson8.urlList;
 
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-        Logic.start();
+
+        // Creating a list of all the URLs in the file.
+        ArrayList<String> everyURLList = Logic.createListOfAllURL();
+
+        // Removing the unnecessary info from the URLs.
+        Logic.normalizeURL(everyURLList);
+
+        // Creating a HashMap that has the URL as the key and the number of times it appears as the value.
+        HashMap<String,Integer> howManyTimesDoesItAppear = Logic.createMapOfAllURL(everyURLList);
+
+        // Sorting the HashMap by the value and printing it out.
+        Logic.sortMapByValueAndPrint(howManyTimesDoesItAppear);
     }
 }
